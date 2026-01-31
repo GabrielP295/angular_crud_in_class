b288d9338d4c028c433b9e56953738f18c085035
GabrielP295 <gabyboyxp@gmail.com>
2026-01-31 11:26:33 -0600

    CRUD practice project during class start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CreateUsers } from './create-users/create-users';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'users',
+        pathMatch: 'full',
+    },
+    {
+        path: 'create-users',
+        component: CreateUsers,
+    },
+];
