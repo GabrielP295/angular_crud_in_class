3af126b94bc49a1e54be9c4528526a5b65e8858e
GabrielP295 <gabyboyxp@gmail.com>
2026-02-13 11:52:11 -0600

    Created read groceries both all and by id; made update grocery by id method as well

--- groceries_app/src/app/services/groceries-database.ts
+++ groceries_app/src/app/services/groceries-database.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Groceries, GroceriesCollection } from '../model/Groceries'
 
-type groceryWithoutId = Omit<Groceries, 'id'>;
+type updateParams = Partial<Omit<Groceries, 'id'>>;
 
 @Injectable({
   providedIn: 'root',
@@ -9,20 +9,7 @@ type groceryWithoutId = Omit<Groceries, 'id'>;
 export class GroceriesDatabase {
   private groceries: GroceriesCollection = [];
 
-  contains(groceryParam: groceryWithoutId) {
-    for (const grocery of this.groceries) {
-      if (grocery === groceryParam) {
-        return grocery;
-      }
-    }
-    return null;
-  }
-
-  createGrocery(grocery: groceryWithoutId): Groceries | null{
-    if(this.contains(grocery)) {
-      return null;
-    }
-
+  createGrocery(grocery: Groceries): Groceries | null {
     const groceryObject = {
       id: crypto.randomUUID(),
       item: grocery.item,
@@ -33,5 +20,28 @@ export class GroceriesDatabase {
     this.groceries.push(groceryObject);
     console.log(`The grocery ${grocery.item} was added.`);
     return groceryObject;
+  }
+
+  readAllGroceries() {
+    return this.groceries;
+  }
+
+  readGroceryById(groceryId: string): Groceries | null {
+    for (const grocery of this.groceries) {
+      if (grocery.id === groceryId) {
+        return grocery;
+      }
+    }
+    return null;
+  }
+
+  updateGrocery(groceryId: string, updateParams: Partial<Omit<Groceries, 'id'>>) {
+    const groceryToUpdate = this.readGroceryById(groceryId);
+
+    if (!groceryToUpdate) {
+      return;
+    }
+
+    
   }
 }
