dee1040d423be51aea2b3afac1568643d79e856b
GabrielDesktop <gabrielpan295@gmail.com>
2026-02-16 22:16:32 -0600

    Implemented display of groceries from in memory database, with the completed UI needed to edit and delete the items

--- groceries_app/src/app/pages/read-groceries/read-groceries.ts
+++ groceries_app/src/app/pages/read-groceries/read-groceries.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GroceriesDatabase } from '../../services/groceries-database';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-read-groceries',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './read-groceries.html',
   styleUrl: './read-groceries.scss',
 })
 export class ReadGroceries {
+  groceryService = inject(GroceriesDatabase);
 
+  readAllGroceries() {
+    return this.groceryService.readAllGroceries();
+  }
 }
